3ee7a8a7f89136ffe960238767a0391427d5041a
Cristian Canedo <criscanedo@protonmail.com>
2018-03-18 15:27:09 -0400

    Test database connect/disconnect

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,22 @@
 
 int main()
 {
-    
+    sqlite3 *db;
+    const char *filename = "C:\\Sqlite\\test.db";
+
+    if (sqlite3_open(filename, &db) != SQLITE_OK) {
+        std::cout << "Connection to database failed.\n";
+    }
+    else {
+        std::cout << "Connection successful\n";
+    }
+
+    if (sqlite3_close(db) != SQLITE_OK) {
+        std::cout << "Connection failed to close\n";
+    }
+    else {
+        std::cout << "Connection closed succesfully\n";
+    }
+
+    return 0;
 }
